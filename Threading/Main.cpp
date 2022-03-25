ab26b787ddf0d0a3eab974cfa12e0a30d3aedb52
Aldo Zulhaq <44134685+maldozulhaq@users.noreply.github.com>
2022-03-25 20:09:06 +0700

    Update Main

--- Threading/Main.cpp
+++ Threading/Main.cpp
@@ -7,38 +7,61 @@ using namespace std;
 
 // inisialisasi mutual exclusion (mutex) dan global variable yang akan digunakan
 mutex m;
+// menggunakan global variable untuk angka yang akan digunakan
+// supaya angka yang akan di print berurutan
 int number = 1;
 
+// function untuk thread
+// dengan parameter sebagai berikut
+// time -> waktu
+// type -> tipe (ganjil atau genap)
+// limit -> jumlah looping yang akan dilakukan
 void print(double time, string type, int limit)
 {
-	int timems = time * 1000; // merubah second menjadi miliseconds
+	// merubah second menjadi miliseconds
+	int timems = time * 1000; 
+
+	// looping output thread
 	for (int i = 0; i < limit; i++)
 	{
-		m.lock(); // menglock variable number sehingga tidak terdapat thread lain yang dapat mengakses variable tersebut
+		// menglock mutex m dan variable number sehingga tidak terdapat thread lain yang dapat mengakses variable tersebut
+		m.lock(); 
+
+		// print angka genap apabila thread bertipe "even"
 		if (type == "even" && number % 2 == 0)
 		{
+			// print angka genap + " even "
 			cout << number << " even " << endl;
+			// increment variable number
 			number++;
 		}
+		// print angka ganjil apabila thread bertipe "odd"
 		else if(type == "odd" && number % 2 != 0)
 		{
+			// print angka ganjil + " odd "
 			cout << number << " odd" << endl;
+			// increment variable number
 			number++;
 		}
-		m.unlock(); // mengunlock sehingga thread lain bisa mengakses kembali
-		this_thread::sleep_for(chrono::milliseconds(timems)); // memberikan delay sebelum loop selanjutnya
+		// mengunlock mutex m sehingga thread lain bisa mengakses kembali
+		m.unlock(); 
+		// memberikan delay sebelum loop selanjutnya
+		this_thread::sleep_for(chrono::milliseconds(timems)); 
 	}
 }
 
 int main()
 {
-	int limit = 10; // limit dari berapa banyak looping yang akan dilakukan
+	// limit dari berapa banyak looping yang akan dilakukan
+	int limit = 10; 
 
-	//inisialisasi thread
+	// Memulai thread odd
 	thread odd(print, (1), "odd", (limit));
+	// Memulai thread odd
 	thread even(print, (2.5), "even", (limit));
 
-	//
+	// Menunggu thread odd untuk selesai
 	odd.join();
+	// Menunggu thread even untuk selesai
 	even.join();
 }
